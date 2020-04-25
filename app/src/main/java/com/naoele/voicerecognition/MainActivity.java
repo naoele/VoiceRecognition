c98bc91a07d125c19f82e0796aad6668bb202cf4
naoele <naoele1dev@gmail.com>
2020-04-25 23:58:52 +0900

    [update] 音声認識 追加

--- app/src/main/java/com/naoele/voicerecognition/MainActivity.java
+++ app/src/main/java/com/naoele/voicerecognition/MainActivity.java
@@ -2,13 +2,100 @@ package com.naoele.voicerecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 1000;
+    private TextView textView;
+
+    // 言語
+    private int lang ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button)findViewById(R.id.btn_start);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 音声認識を開始
+                speech();
+            }
+        });
+    }
+
+    private void speech(){
+        // 音声認識の　Intent インスタンス
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra("android.speech.extras.SPEECH_INPUT_COMPLETE_SILENCE_LENGTH_MILLIS", 10000);
+        intent.putExtra("android.speech.extra.GET_AUDIO_FORMAT", "audio/AMR");
+        intent.putExtra("android.speech.extra.GET_AUDIO", true);
+        intent.putExtra("android.speech.extra.PROMPT", "マイクに向かって話してください。");
+
+        if(lang == 0){
+            // 日本語
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.JAPAN.toString() );
+        }
+        else if(lang == 1){
+            // 英語
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH.toString() );
+        }
+        else if(lang == 2){
+            // Off line mode
+            intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE, true);
+        }
+        else{
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        }
+
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 100);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "音声を入力");
+
+        try {
+            // インテント発行
+            startActivityForResult(intent, REQUEST_CODE);
+        }
+        catch (ActivityNotFoundException e) {
+            Log.e("音声認識テスト", "エラー", e);
+            textView.setText(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            // 認識結果を ArrayList で取得
+            ArrayList<String> candidates =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            if(candidates.size() > 0) {
+                // 認識結果候補で一番有力なものを表示
+                textView.setText( candidates.get(0));
+            }
+
+
+        }
     }
 }
